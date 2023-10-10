5ea3b0eeb907493035740197db900cbd7a1ed661
Nicolae Pastrama <nicolae.pastrama@student.vinci.be>
2023-10-10 14:33:47 +0200

    2.3.1 : event object & form

---
+++ exercices/2.3.1/ihm/index.js
@@ -0,0 +1,13 @@
+const btn1 = document.querySelector("#btn1");
+const form1 = document.querySelector("#form1");
+const wish = document.querySelector("#wish");
+const wishsubmited = document.querySelector("#wishSubmited");
+
+const send = (e) =>{
+    form1.style.display = "none";
+    wish.innerText = "Le souhait est : " + wishsubmited.value; 
+    e.preventDefault();
+};
+
+btn1.addEventListener('click', send);
+
