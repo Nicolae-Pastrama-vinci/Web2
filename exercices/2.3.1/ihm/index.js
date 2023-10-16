79c624d6ae756e679c1a50b35f4accac4fd16709
Nicolae-Pastrama-vinci <nicolae.pastrama@student.vinci.be>
2023-10-16 11:44:39 +0200

    a

--- exercices/2.3.1/ihm/index.js
+++ exercices/2.3.1/ihm/index.js
@@ -4,7 +4,7 @@ const wish = document.querySelector("#wish");
 const wishsubmited = document.querySelector("#wishSubmited");
 
 const send = (e) =>{
-    form1.style.display = "none";
+    form1.style.display = "none";   
     wish.innerText = "Le souhait est : " + wishsubmited.value; 
     e.preventDefault();
 };
